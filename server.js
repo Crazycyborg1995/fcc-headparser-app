715c0c8fd39c985b94ef9b61d82dfe3ff3176c94
Crazycyborg1995 <mhdafsalvp@gmail.com>
2019-03-13 15:20:31 +0530

    final

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/user', (req, res) => {
+  let ipaddress = req.ip;
+  let language = req.headers['accept-language'].split(',')[0];
+  let software = req.headers['user-agent'].match(/\((.+?)\)/)[1];
+  console.log(ipaddress, software);
+  // res.send({ ipaddress, language, software });
+  res.status(200).json({ ipaddress, language, software });
+});
+app.listen(port, () => {
+  console.log(`Server started on port : ${port}`);
+});
